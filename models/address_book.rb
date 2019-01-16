9c2c7f56da42e0023267cfc1578567b8fb2c8b0e
Jordan Hower <jhower91@gmail.com>
2019-01-16 15:18:21 -0700

    Issues with menu

---
+++ models/address_book.rb
@@ -0,0 +1,49 @@
+require_relative 'controllers/menu_controller'
+
+ # #4
+ menu = MenuController.new
+ # #5
+ system "clear"
+ puts "Welcome to AddressBloc!"
+ # #6
+ menu.main_menu
+
+
+
+
+# #8
+require_relative 'entry'
+
+class AddressBook
+attr_reader :entries
+
+def initialize
+     @entries = []
+   end
+
+  def remove_entry(name, phone, email)
+    delete_entry = nil
+
+    @entries.each do |entry|
+      if name == entry.name && phone == entry.phone_number && email == entry.email
+        delete_entry = entry
+      end
+ end
+
+    @entries.delete(delete_entry)
+  end
+
+    def add_entry(name, phone_number, email)
+      # #9
+      index = 0
+      entries.each do |entry|
+      # #10
+        if name < entry.name
+          break
+        end
+        index+= 1
+      end
+      # #11
+      entries.insert(index, Entry.new(name, phone_number, email))
+    end
+  end
